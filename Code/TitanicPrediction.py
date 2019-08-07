8eb37fe38dec102bae92b746b9d7339cf4c3c794
abla1 <50546508+abla1@users.noreply.github.com>
2019-08-06 22:35:50 -0400

    first add to code

--- Code/TitanicPrediction.py
+++ Code/TitanicPrediction.py
@@ -1,1 +1,40 @@
 [Code goes here]
+######## Imports ########
+from tqdm import tqdm
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords,wordnet
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize, pos_tag
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import StratifiedKFold
+from IPython.display import Image
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+k_fold = KFold(n_splits=10, shuffle=True, random_state=0)
+#######Import Data##########
+TrainFile = pd.read_csv(r"C:\Users\enam1\Documents\dataanalysis\Titanic\titanic_train.csv")
+TestFile = pd.read_csv(r"C:\Users\enam1\Documents\dataanalysis\Titanic\titanic_test.csv")
+######## Data Discovery#########
